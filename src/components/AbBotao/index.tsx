ebb45ac0f2a8d7cb4599176227ccb2f212d875b2
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-03-27 15:45:16 -0300

    component estilizado + example

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,11 +1,24 @@
 import React from "react"
+import styled from "styled-components"
 
-const AbBotao = () => {
+const BotaoStyled = styled.button`
+    cursor: pointer;
+    background-color: #EB9B00;
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    color: #FFF;
+    font-size: 20px;
+    transition: .3s;
+    &:hover {
+        background-color: #B87900;
+        border-color: #B87900;
+    }
+`
+
+export const AbBotao = () => {
     return(
-        <button>
+        <BotaoStyled>
             Clique aqui!
-        </button>
+        </BotaoStyled>
     )
 }
-
-export default AbBotao
